592816431a1f9f09c94775044f5866fbd86a2bd2
JannikNieb <jannik.niebling@gmx.de>
2023-12-22 14:27:47 +0100

    Add option to sample uniformly - add option to sample from uniform distribution (no training process) - add learning rate decay if on plateau

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
 import numpy as np
 import time
 import torch
+import sys
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 import matplotlib.pyplot as plt
 from torch.autograd import grad
+import uuid  # for generating random uuids
+
 
 from oscillatory import OscillatoryFlows
 from oscillatory import CustomDataset
 from oscillatory import FeedForwardNet
 from oscillatory import CustomLoss
 
+# torch.set_float32_matmul_precision("medium")  # decrease precision of matrix multiplications to increase speed
+
 _h = 4
 scale = 3
 
 data_size = 10 ** 5  # Replace with the desired number of data
 batch_size = 10 ** 3  # Define the batch size
+test_size = data_size * 2
 
-dimensions = [1, 100, 1]
+test_batch_size = int(batch_size * (test_size / data_size))
 
-num_epochs = 10
-learning_rate = 0.025
+dimensions = [1, 50, 1]
+
+num_epochs = 200
+learning_rate = 1
+
+data_folder = 'data/trash'
+
+normal_dist = False
+
 
 # Run the code on GPU or MPS, if possible, otherwise us CPU
 device = (
@@ -32,28 +45,51 @@ device = (
 )
 print(f"Using {device} device")
 
-net = OscillatoryFlows(_h, data_size, device)
+if len(sys.argv) > 1:
+    id = sys.argv[1]
+else:
+    id = str(uuid.uuid1())
+
+print(id)
+
+# torch.seed()
+net = OscillatoryFlows(_h, device, id, normal_dist)
 
 
 # Define your custom dataset using the data and labels
-x_train, x_test = net.generate_samples()
-train_dataset = CustomDataset(x_train, net.f(x_train, _h))
-test_dataset = CustomDataset(x_test, net.f(x_test, _h))
+x_train = net.generate_samples(data_size)
+x_test = net.generate_samples(test_size)
+x_test_large = net.generate_samples(10**8)
+
+if normal_dist:
+    train_dataset = CustomDataset(x_train, net.compute_oscil(x_train, _h),
+                                  weight_labels=net.compute_weight(x_train, inverse=True))
+    test_dataset = CustomDataset(x_test, net.compute_oscil(x_test, _h),
+                                 weight_labels=net.compute_weight(x_test, inverse=True))
+else:
+    train_dataset = CustomDataset(x_train, net.f(x_train, _h))
+    test_dataset = CustomDataset(x_test, net.f(x_test, _h))
+    large_test_dataset = CustomDataset(x_test_large, net.f(x_test, _h))
+
 
 # Create the train_loader
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=int(batch_size * 0.1), shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=test_batch_size, shuffle=True)
+large_test_loader = DataLoader(large_test_dataset, batch_size=10**5, shuffle=True)
 
 
 model = FeedForwardNet(dimensions).to(device)
 loss_criterion = CustomLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)  # Use Stochastic Gradient descend to train
+lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=7,
+                                                          threshold=0.0001, threshold_mode='abs')
 
+x_test_sort = x_test[:test_batch_size].sort()[0]  # returns sorted samples (.sort() returns a tuple (values, indices))
+# x_test_sort = x_test.sort()[0]  # returns sorted samples (.sort() returns a tuple (values, indices))
 
-x_test_sort = x_test[:batch_size].sort()[0]  # returns sorted samples (.sort() returns a tuple (values, indices))
-
-test_hist = torch.empty(num_epochs).to(device)
-train_hist = torch.empty(num_epochs).to(device)
+test_hist = torch.empty(num_epochs, device=device)
+train_hist = torch.empty(num_epochs, device=device)
+integral_hist = torch.empty(num_epochs, device=device).tolist()
 results = []  # array containing all integral estimates
 learned_graph =[net.f(x_test_sort.detach().cpu(), _h).tolist()]  # array containing all trained sample data
 
@@ -62,20 +98,30 @@ for t in range(num_epochs):
     print(f"Epoch {t+1}\n-------------------------------")
     model, loss = net.training(train_loader, model, optimizer, loss_criterion)
     train_hist[t] = torch.tensor(loss)
-    test_hist[t] = torch.tensor(net.testing(test_loader, model, loss_criterion))
-    if t % 10 == 0:
+    test_hist[t], prediction, integral_hist[t] = net.testing(test_loader, model, loss_criterion, scale)
+    lr_scheduler.step(test_hist[-1])  # adjust learning rate
+    print(optimizer.param_groups[0]["lr"])
+    if t % 10 == 0 and t > 1:  # save model every 10 epochs
         outputs = model(x_test_sort[:, None])
         grads, = grad(outputs, x_test_sort, grad_outputs=torch.ones_like(outputs), create_graph=True)
-        prediction = 0.5 * (net.f(x_test_sort, _h) + torch.flatten(net.f(outputs.detach(), _h)) * torch.abs(grads))
+        prediction = 0.5  * (net.f(x_test_sort, _h) + torch.flatten(net.f(outputs.detach(), _h)) * torch.abs(grads))
         learned_graph.append(prediction.detach().cpu().numpy().tolist())
         # ax2.plot(x_test_sort.detach().cpu(), prediction.detach().cpu(), label=f"epoch {t}")
         results.append((2 * scale * torch.mean(prediction)).detach().cpu())
+        net.save_model(data_folder, test_hist.tolist(), model, learning_rate, dimensions, learned_graph, test_size,
+                       x_test_sort.detach().cpu().tolist(), t,
+                       f"{int(round((time.time() - start_time) / 60))} min", integral_hist)
+training_time = int(round((time.time() - start_time) / 60))
+print(f"time for training: {training_time}min")
 
-print(f"time for training: {time.time() - start_time:.0f}s")
+integral_hist[-1] = net.testing(large_test_loader, model, loss_criterion, scale)[-1]
+print(integral_hist[-1])
 
-saved = net.save_model('data', test_hist.tolist(), model, learning_rate, dimensions, learned_graph, scale,
-                       x_test_sort.detach().cpu().tolist())
+saved = net.save_model(data_folder, test_hist.tolist(), model, learning_rate, dimensions, learned_graph, scale,
+                       x_test_sort.detach().cpu().tolist(), num_epochs, training_time, integral_hist)
 
 print(saved['id'])
+fig = net.plot_results(data_folder)
+plt.show()
 
 # grads, = grad(outputs, x_test_sort, grad_outputs=torch.ones_like(outputs), create_graph=True)
--- oscillatory.py
+++ oscillatory.py
@@ -14,15 +14,16 @@ from tqdm import tqdm  # for displaying progress bars in the training
 
 # Define a custom dataset that inherits from the Dataset class
 class CustomDataset(Dataset):
-    def __init__(self, data, labels):
+    def __init__(self, data, labels, weight_labels=[1]):
         self.data = data.requires_grad_()
         self.labels = labels
+        self.weight_labels = torch.ones(len(labels)) if len(weight_labels)==1 else weight_labels
 
     def __len__(self):  # Returns the number of samples in the dataset
         return len(self.labels)
 
     def __getitem__(self, idx):  #  Loads and returns a sample from the dataset at the given index idx
-        return self.data[idx], self.labels[idx]
+        return self.data[idx], self.labels[idx], self.weight_labels[idx]
 
 
 class FeedForwardNet(nn.Module):
@@ -44,7 +45,7 @@ class CustomLoss(nn.Module):
     def __init__(self):
         super().__init__()
 
-    def forward(self, pred, target, grad):
+    def forward(self, pred, target, grad, weight=torch.tensor(1)):
         """
 
         :param pred: f(model outputs)
@@ -53,34 +54,40 @@ class CustomLoss(nn.Module):
         :return:
         """
         # Calculate the (quadratic) loss
-        loss = (target + pred * torch.abs(grad)) ** 2
+        loss = (target + weight * pred * torch.abs(grad)) ** 2
         return 0.25 * loss.mean()
 
 
 class OscillatoryFlows:
-    def __init__(self, h, data_size, device):
+    def __init__(self, h, device, id, normal_dist=False):
         self.h = h
-        self.data_size = data_size
         self.device = device
-        self.id = str(uuid.uuid1())  # custom uuid to identify the trained network (esp. for saving)
+        self.id = id  # custom uuid to identify the trained network (esp. for saving)
+        self.normal_dist = normal_dist
+        self.seed = torch.seed()
 
-    def generate_samples(self, batch_size=0, scale=3):
-        if batch_size == 0:
-            batch_size = 0.01 * self.data_size
-        data = (torch.rand(self.data_size) - 0.5) * 2 * scale
-        train_size = int(self.data_size - self.data_size * 0.1)
-        x_train = data[:train_size].to(self.device)
-        x_test = data[train_size:].to(self.device)
-        return x_train, x_test
+    def generate_samples(self, sample_size, scale=3):
+        """
+        generates 2 uniformly distributed samples in [-scale, scale]
+        :param scale: size of the interval which is sampled from
+        :return:
+        """
+        if self.normal_dist:
+            x = torch.randn(sample_size, device=self.device)  # generate normal distributed function
+        else:
+            x = (torch.rand(sample_size, device=self.device) - 0.5) * 2 * scale  # generate uniform distribution
+        return x
 
     def compute_weight(self,
-            x: torch.Tensor):
+                       x: torch.Tensor,
+                       inverse=False):
         """
         exponential part of the function
         :param x:
         :return:
         """
-        return torch.exp(-0.5 * x**2) / np.sqrt(2 * np.pi)
+        weight = 0.5 * x**2 if inverse else torch.exp(-0.5 * x ** 2)
+        return weight
 
     def compute_oscil(self,
             x: torch.Tensor,
@@ -96,7 +103,8 @@ class OscillatoryFlows:
     def f(self,
             x: torch.Tensor,
             h: float):
-        return torch.flatten(self.compute_weight(x) * self.compute_oscil(x, h))
+        return (2 * torch.pi)**-0.5 * torch.flatten(self.compute_weight(x) * self.compute_oscil(x, h))
+
 
     def compute_integral_analytic(self,
             h: float):
@@ -106,6 +114,8 @@ class OscillatoryFlows:
         :return:
         """
         return np.exp(-h**2 / 2)
+
+
     def training(self,
             train_loader,
             model,
@@ -113,41 +123,60 @@ class OscillatoryFlows:
             loss_criterion):
 
         model.train()  # set model to training mode (unnecessary here, but good practice)
-        for i, (inputs, labels) in enumerate(tqdm(train_loader)):
+        for i, (inputs, labels, weight_labels) in enumerate(tqdm(train_loader)):
             optimizer.zero_grad()  # Reset the gradients from the previous iteration
+            # with torch.autocast(device_type=self.device):
             outputs = model(inputs[:,None])
             grads, = grad(outputs, inputs, grad_outputs=torch.ones_like(outputs), create_graph=True)
-            pred = self.f(outputs, self.h)
-            loss = loss_criterion(pred, labels, grads)
+            if self.normal_dist:
+                pred = self.compute_oscil(outputs, self.h)
+                weight = torch.exp(0.5 * inputs**2 - 0.5 * outputs**2) #* self.compute_weight(outputs)
+                loss = loss_criterion(pred, labels, grads, weight=weight)
+            else:
+                pred = self.f(outputs, self.h)
+                loss = loss_criterion(pred, labels, grads)
+
 
             # l2_norm = sum(p.pow(2.0).sum() for p in model.parameters())
             # l1_norm = sum(p.abs().sum() for p in model.parameters())
 
             loss.backward()  # Calculate the gradient with respect to each parameter
             optimizer.step()  # Adjust the network parameters using the gradients calculated by .backward()
-        print(f"training loss: {loss.item():>7f}")
+        print(f"training loss: {loss.detach():>7f}")
         return model, loss.cpu().detach().numpy()
 
-    def testing(self, test_loader, model, loss_criterion):
+
+    def testing(self, test_loader, model, loss_criterion, scale):
         model.eval()  # Set the model to evaluation mode (unnecessary here, but good practice)
         size = len(test_loader.dataset)
         num_batches = len(test_loader)
-        test_loss, correct = 0, 0
+        test_loss, integral_estimate = 0, 0
 
         # Evaluating the model with torch.no_grad() ensures that no gradients are computed during test mode
         # also serves to reduce unnecessary gradient computations and memory usage for tensors with requires_grad=True
         # with torch.no_grad():
-        for inputs, labels in test_loader:
+        for inputs, labels, weight_labels in tqdm(test_loader):
             # inputs.requires_grad = True  # Set requires_grad property of the tensors, to use autograd on them later
             outputs = model(inputs[:, None])
             grads, = grad(outputs, inputs, grad_outputs=torch.ones_like(outputs), create_graph=True)
-            pred = self.f(outputs, self.h)
+            if self.normal_dist:
+                pred = self.compute_oscil(outputs, self.h)
+                weight = torch.exp(0.5 * inputs ** 2 - 0.5 * outputs ** 2)
+                test_loss += loss_criterion(pred, labels, grads, weight=weight)
+                prediction = (8 * torch.pi)**-0.5 * (self.f(inputs, self.h) +
+                                        torch.flatten(self.f(outputs.detach(), self.h)) * torch.abs(grads))
+            else:
+                pred = self.f(outputs, self.h)
+                test_loss += loss_criterion(pred, labels, grads).item()
+                # x_test_sort = test_loader[0][0].sort()[0]
+                prediction = 0.5 * (self.f(inputs.sort()[0], self.h) +
+                                torch.flatten(self.f(outputs.sort()[0].detach(), self.h)) * torch.abs(grads.sort()[0]))
+                integral_estimate += 2 * scale * torch.mean(prediction.detach().cpu())
 
-            test_loss += loss_criterion(pred, labels, grads).item()
-
+        integral_estimate /= num_batches
         test_loss /= num_batches
         print(f"Test Error: \n Avg loss: {test_loss:>8f} \n")
-        return test_loss
+        return test_loss, prediction, float(integral_estimate)
 
     def save_model(self,
            path: str,
@@ -156,8 +185,11 @@ class OscillatoryFlows:
            learning_rate: float,
            dimensions: list,
            plot_results: list,
-           scale: int,
-           samples: list) -> dict:
+           test_size: int,
+           samples: list,
+           epochs: int,
+           time: str,
+           integral_estimates: list) -> dict:
         """
 
         :param samples: original sample distribution in range (-scale, scale)
@@ -174,19 +206,26 @@ class OscillatoryFlows:
         id = self.id
         torch.save(model, path + '/' + id)
         ana_int = self.compute_integral_analytic(self.h)
-        calc_int = 2 * scale * np.mean(plot_results[0])
+        calc_int = float(integral_estimates[-1])
+        org_int = float(integral_estimates[0])
         save_dict = {
             'id': id,
             'loss': loss[-1],
-            'integral result': round(2 * scale * np.mean(plot_results[-1]), 6),
-            'original integral approx': round(calc_int, 6),
+            'integral result': round(calc_int, 6),
+            'original integral approx': round(org_int, 6),
             'relative error': round((calc_int - ana_int) / ana_int, 2),
-            'loss history': loss,
             'learning rate': learning_rate,
-            'number of layers': len(dimensions) - 2,
+            # 'number of layers': len(dimensions) - 2,
             'layer dimensions': dimensions,
+            'number of (test) samples': len(plot_results[0]),
+            'distributing': "normal" if self.normal_dist else "uniform",
+            'epochs': epochs,
+            'time': time,
+            'seed': self.seed,
+
+            'integral history': list(integral_estimates),
+            'loss history': loss,
             'learned graph': plot_results,
-            'number of samples': len(plot_results[0]),
             'samples': samples}
         with open(path + '/' + id + '.json', 'w', encoding='utf-8') as file:
             json.dump(save_dict, file)
@@ -199,7 +238,7 @@ class OscillatoryFlows:
             data_dict = json.load(file)
 
         cmap = cm.get_cmap('inferno', int(round(len(data_dict['learned graph'])*1.3)))
-        fig, (ax1, ax2) = plt.subplots(1, 2)
+        fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
 
         ax1.plot(data_dict['loss history'])
         ax1.set_xlabel('epoch')
@@ -207,8 +246,10 @@ class OscillatoryFlows:
 
         # ax2.plot(data_dict[0], self.f(data_dict[0], self.h), label=str(i))
         for i, graph in enumerate(data_dict['learned graph']):
-            if i==0:
-                graph = self.f(torch.tensor(graph), self.h).numpy()
             ax2.plot(data_dict['samples'], graph, label=f"epoch {i * 10}", c=cmap(i))
         # ax2.legend()
+
+        ax3.plot(data_dict['integral history'])
+        ax3.hlines(self.compute_integral_analytic(self.h), 0, len(data_dict['integral history']), color='grey')
+
         return fig
